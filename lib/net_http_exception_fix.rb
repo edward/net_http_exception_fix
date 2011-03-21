4211a0a710193d0420deae61b14e739541e5c3b4
Edward Ocampo-Gooding <edward@edwardog.net>
2011-03-20 21:47:19 -0400

    Fix git screwup/typo

--- lib/net_http_exception_fix.rb
+++ lib/net_http_exception_fix.rb
@@ -2,7 +2,7 @@ require 'net/http'
 
 module Net::HTTPBroken; end
 
-[Timeout::Error, Errno::ETIMEDOUT, Errno::EINVAL, Errno::ECONNRESET, 
+[Timeout::Error, Errno::ETIMEDOUT, Errno::EINVAL, Errno::ECONNRESET,
  Errno::ECONNREFUSED, EOFError, Net::HTTPBadResponse,
  Net::HTTPHeaderSyntaxError, Net::ProtocolError].each do |m|
    m.send(:include, Net::HTTPBroken)
