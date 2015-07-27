9302575688955d1c1012d9ead31f21b12e54a651
Edward Ocampo-Gooding <hello@edward.bio>
2015-07-27 10:38:17 -0400

    Whitespace

--- lib/net_http_exception_fix.rb
+++ lib/net_http_exception_fix.rb
@@ -5,5 +5,5 @@ module Net::HTTPBroken; end
 [Timeout::Error, Errno::ETIMEDOUT, Errno::EINVAL, Errno::ECONNRESET,
  Errno::ECONNREFUSED, EOFError, Net::HTTPBadResponse,
  Net::HTTPHeaderSyntaxError, Net::ProtocolError].each do |m|
-   m.send(:include, Net::HTTPBroken)
- end
+  m.send(:include, Net::HTTPBroken)
+end
